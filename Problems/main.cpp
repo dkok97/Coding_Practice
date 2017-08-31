71ee8b573a95b4bfc7a0dff8aff19b05e4c03c28
Dinkar Khattar <dinkar@Dinkars-MacBook-Pro.local>
2017-08-31 03:32:35 +0300

    Initial Commit

---
+++ Problems/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problems
+//
+//  Created by Dinkar Khattar on 8/31/17.
+//  Copyright © 2017 Dinkar Khattar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
